6e81c83bace3bd65ad1c2180dd4d7a8261612c5f
Geferson Eurico dos Santos <gefersoneds@gmail.com>
2020-08-06 18:25:38 -0300

    Parte 2 Criado paginas de cadastro e video

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Logo4 from '../../assets/img/Logo4.png';
 import './Menu.css';
 import Button from '../Button';
@@ -7,11 +8,12 @@ import Button from '../Button';
 function Menu () {
     return (
         <nav className = "Menu">
-            <a href = "/">
+            <Link to = "/">
                 <img className = "Logo" src = {Logo4} alt = "AluraFlix logo" />
-            </a>
+            </Link>
             
-            <Button as = "a" className = "ButtonLink" href = "/">
+            <Button as = {Link} className = "ButtonLink" to
+             = "/cadastro/video">
                 Novo Vídeo
             </Button>
         </nav>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import Home from './pages/cadastro/home';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/video'
 
 ReactDOM.render(
   <BrowserRouter>
-     <App />
-  </BrowserRouter>,
+  <Switch>
+    <Route path = "/" component = {Home} exact/>
+    <Route path = "/cadastro/video" component = {CadastroVideo}/>
+    <Route path = "/" component = {() => <div>Página 404</div>}/>
 
-/*   <React.StrictMode>
-    <App />
-  </React.StrictMode>, */
+  </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
